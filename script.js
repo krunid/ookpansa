9c287fa157d7c1aee1b6b17d697334f137baaef9
krunid <64851441+krunid@users.noreply.github.com>
2023-10-23 12:21:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     height: "390",
     width: "640",
-    videoId: "6EZmOM767HQ",
+    videoId: "46U_I4QMV-w",
     playerVars: { 
       rel: 0,
       autoplay: 1, 
